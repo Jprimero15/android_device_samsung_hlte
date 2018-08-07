e5a8d7c19512f6eade2e11080b39d9a28372fdce
Jprimero15 <jprimero155@gmail.com>
2018-08-07 11:03:19 +0000

    dot-ify

--- dot.mk
+++ dot.mk
@@ -17,9 +17,9 @@
 
 $(call inherit-product, device/samsung/hlte/full_hlte.mk)
 
-# Inherit some common AOSiP stuff.
-$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+# Inherit some common DotOS  stuff.
+$(call inherit-product, vendor/dot/config/common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hlte
-PRODUCT_NAME := aosip_hlte
+PRODUCT_NAME := dot_hlte
