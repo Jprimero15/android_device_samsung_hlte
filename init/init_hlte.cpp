8cfee92d9690a3493493a8cdf15c0c7b55d9037d
Joshua Primero <jprimero155@gmail.com>
2019-10-16 11:34:58 -0700

    init: Override Everything

--- init/init_hlte.cpp
+++ init/init_hlte.cpp
@@ -47,22 +47,26 @@
 using android::base::GetProperty;
 using android::init::property_set;
 
-void property_override(char const prop[], char const value[])
-{
-    prop_info *pi;
+// copied from build/tools/releasetools/ota_from_target_files.py
+// but with "." at the end and empty entry
+std::vector<std::string> ro_product_props_default_source_order = {
+    ".",
+    "product.",
+    "product_services.",
+    "odm.",
+    "vendor.",
+    "system.",
+};
 
-    pi = (prop_info*) __system_property_find(prop);
-    if (pi)
+void property_override(char const prop[], char const value[], bool add = true)
+{
+    auto pi = (prop_info *) __system_property_find(prop);
+
+    if (pi != nullptr) {
         __system_property_update(pi, value, strlen(value));
-    else
+    } else if (add) {
         __system_property_add(prop, strlen(prop), value, strlen(value));
-}
-
-void property_override_dual(char const system_prop[],
-        char const vendor_prop[], char const value[])
-{
-    property_override(system_prop, value);
-    property_override(vendor_prop, value);
+    }
 }
 
 void set_rild_libpath(char const *variant)
@@ -106,19 +110,29 @@ void vendor_load_properties() {
 
     std::string bootloader = GetProperty("ro.bootloader", "");
 
+    const auto set_ro_product_prop = [](const std::string &source,
+            const std::string &prop, const std::string &value) {
+        auto prop_name = "ro.product." + source + prop;
+        property_override(prop_name.c_str(), value.c_str(), false);
+    };
+
     if (bootloader.find("N9005") == 0) {
         /* hltexx */
-        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/hltexx/hlte:5.0/LRX21V/N9005XXSGBRI2:user/release-keys");
+       for (const auto &source : ro_product_props_default_source_order) {
+            set_ro_product_prop(source, "fingerprint", "samsung/hltexx/hlte:5.0/LRX21V/N9005XXSGBRI2:user/release-keys");
+            set_ro_product_prop(source, "device", "hlte");
+            set_ro_product_prop(source, "model", "SM-9005");
+        }
         property_override("ro.build.description", "hltexx-user 5.0 LRX21V N9005XXSGBRI2 release-keys");
-        property_override_dual("ro.product.model", "ro.vendor.product.model", "SM-N9005");
-        property_override_dual("ro.product.device", "ro.vendor.product.device", "hlte");
         gsm_properties("gsm");
     } else if (bootloader.find("N900P") == 0) {
         /* hltespr - Sprint */
-        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/hltespr/hltespr:5.0/LRX21V/N900PVPSEPL1:user/release-keys");
+       for (const auto &source : ro_product_props_default_source_order) {
+            set_ro_product_prop(source, "fingerprint", "samsung/hltespr/hltespr:5.0/LRX21V/N900PVPSEPL1:user/release-keys");
+            set_ro_product_prop(source, "device", "hltespr");
+            set_ro_product_prop(source, "model", "SM-900P");
+        }
         property_override("ro.build.description", "hltespr-user 5.0 LRX21V N900PVPSEPL1 release-keys");
-        property_override_dual("ro.product.model", "ro.vendor.product.model", "SM-N900P");
-        property_override_dual("ro.product.device", "ro.vendor.product.device", "hltespr");
         cdma_properties("Sprint", "310120", "8", "1", "spr");
     } else {
         gsm_properties("gsm");
