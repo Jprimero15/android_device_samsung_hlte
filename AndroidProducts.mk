4962fc90f4545bcdbab15826569bd5e800e2bdf4
Joshua Primero <jprimero155@gmail.com>
2020-04-17 13:33:26 +0000

    hlte: Xtended boi
    Signed-off-by: Joshua Primero <jprimero155@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_hlte.mk
+    $(LOCAL_DIR)/xtended_hlte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_hlte-user \
-    lineage_hlte-userdebug \
-    lineage_hlte-eng
+    xtended_hlte-user \
+    xtended_hlte-userdebug \
+    xtended_hlte-eng
